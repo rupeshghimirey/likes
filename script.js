4953a05c7343287fc43c5e38b54cc716476bd522
rupeshghimirey <rupesh.ghimirey@gmail.com>
2021-09-20 23:41:52 -0400

    likes initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var NeiltotalLikes = 9;
+var currentNeilLikes = document.getElementById("NeiltotalLikes");
+currentNeilLikes.innerText = NeiltotalLikes;
+
+function increaseLikesNeil() {
+    NeiltotalLikes++;
+    currentNeilLikes.innerText = NeiltotalLikes;
+}
+
+var NicholetotalLikes = 12;
+var currentNicholeLikes = document.getElementById("NicholetotalLikes");
+
+currentNicholeLikes.innerText = NicholetotalLikes;
+
+function increaseLikesNichole() {
+    NicholetotalLikes++;
+    currentNicholeLikes.innerText = NicholetotalLikes;
+}
+
+var JimtotalLikes = 9;
+var currentJimLikes = document.getElementById("JimtotalLikes");
+
+currentJimLikes.innerText = JimtotalLikes;
+
+function increaseLikesJim() {
+    JimtotalLikes++;
+    currentJimLikes.innerText = JimtotalLikes;
+}
+
